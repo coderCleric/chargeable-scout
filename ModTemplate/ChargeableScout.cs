6abc7c0766d1fe89225fb37f9d45bc045c626be2
coderCleric <wescamphouse@gmail.com>
2022-10-21 20:59:21 -0600

    Made it so that the little arrow on the launcher moves, and give the option to disable the charge popup.

--- ModTemplate/ChargeableScout.cs
+++ ModTemplate/ChargeableScout.cs
@@ -1,4 +1,5 @@
 ﻿//Special thanks to PacificEngine, as my percentage display code is largely based off of their Cheats Mod code
+
 using OWML.Common;
 using OWML.ModHelper;
 using UnityEngine.InputSystem;
@@ -13,11 +14,15 @@ namespace ChargeableScout
         private float maxMultiplier;
         private float secToCharge;
         public static bool kickbackEnabled;
+        private bool displayCharge;
 
         //Other variables
         public static float launchMultiplier = 1;
         public static bool launcherPrimed = false;
         private ScreenPrompt percentDisplay = new ScreenPrompt("");
+        private const float MAX_ARROW_ROTATION = 510f;
+        private Transform arrowTransform = null;
+        private float fakeAngle = 315.3015f;
 
 
         /**
@@ -68,22 +73,30 @@ namespace ChargeableScout
                 maxMultiplier = tmp;
             }
 
-            //Charge the probe and display percent if it's primed
+            //Do mod stuff if the scout launcher is primed
             if (launcherPrimed)
             {
+                //Save the arrow if we don't have it
+                if(this.arrowTransform == null && (Locator.GetToolModeSwapper()._equippedTool as PlayerProbeLauncher) != null)
+                {
+                    this.arrowTransform = Locator.GetToolModeSwapper()._equippedTool.transform.Find("Props_HEA_ProbeLauncher").Find("PressureGauge_Arrow");
+                }
+
                 //Remove the priming if the launcher is not equipped
                 if ((Locator.GetToolModeSwapper()._equippedTool as ProbeLauncher) == null)
                     launcherPrimed = false;
 
                 else
                 {
+
                     //Up the launch multiplier
                     //Special case for a secToCharge of 0
-                    if(secToCharge <= 0)
+                    if (secToCharge <= 0)
                     {
                         launchMultiplier = maxMultiplier;
                     }
-                    else {
+                    else
+                    {
                         launchMultiplier += ((maxMultiplier - minMultiplier) / secToCharge) * Time.deltaTime;
                         launchMultiplier = Mathf.Min(launchMultiplier, maxMultiplier);
                     }
@@ -97,13 +110,17 @@ namespace ChargeableScout
                     //Update the prompt and make it visible
                     //Special case if max == min
                     float percentCharge;
-                    if(maxMultiplier == minMultiplier)
+                    if (maxMultiplier == minMultiplier)
                         percentCharge = 100;
-                    else 
+                    else
                         percentCharge = Mathf.Round((launchMultiplier - minMultiplier) / (maxMultiplier - minMultiplier) * 100);
 
                     this.percentDisplay.SetText($"Launcher Charge: {percentCharge}%\n ({Mathf.Round(launchMultiplier * 100) / 100})X Force");
-                    this.percentDisplay.SetVisibility(true);
+                    this.percentDisplay.SetVisibility(displayCharge);
+
+                    //Rotate the pressure arrow
+                    this.fakeAngle = ((MAX_ARROW_ROTATION - 315.3015f) * (percentCharge / 100f)) + 315.3015f;
+                    this.arrowTransform.localEulerAngles = new Vector3(fakeAngle, 90f, 270f);
 
                     //Rumble the controller for style
                     RumbleManager.Pulse(0.05f, 0.05f, 0.05f);
@@ -115,6 +132,13 @@ namespace ChargeableScout
             {
                 launchMultiplier = minMultiplier;
                 this.percentDisplay.SetVisibility(false);
+
+                //Set the arrow back
+                if (this.arrowTransform != null)
+                {
+                    this.fakeAngle = Mathf.Max(315.3015f, this.fakeAngle - ((MAX_ARROW_ROTATION - 315.3015f) * Time.deltaTime * 4));
+                    this.arrowTransform.localEulerAngles = new Vector3(this.fakeAngle, 90f, 270f);
+                }
             }
 
             //Try and fire the scout when the button is released
@@ -135,6 +159,7 @@ namespace ChargeableScout
             maxMultiplier = config.GetSettingsValue<float>("maxMult");
             secToCharge = config.GetSettingsValue<float>("timeToCharge");
             kickbackEnabled = config.GetSettingsValue<bool>("scalingPushback");
+            displayCharge = config.GetSettingsValue<bool>("displayCharge");
         }
     }
 }
